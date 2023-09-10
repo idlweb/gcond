739dd013b1b020abd05b59de42016edda7a0ae70
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 11:23:13 +0200

    .

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@
 
 #from . import controllers
 from . import models
-from . import views
-from . import security
+#from . import views
+#from . import security
 #from . import demo
 #from . import wizard
 #from . import report
