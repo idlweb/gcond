4a68f46cb56169962ee31c14e5d6cdfa4bc25c63
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 18:15:24 +0200

    .

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,9 @@
 #from . import tabelle_ripartizione
 #from . import quote_di_partecipazione
 #from . import account_condominio
-#from . import account_condominio_table
-#from . import condomino
-#from . import account_move_condominio
+from . import account_condominio_table
+from . import condomino
+from . import account_move_condominio
 
 
 
