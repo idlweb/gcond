5a295845cae34f77ea18fd1ddc045a1e19ea4093
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 18:56:01 +0200

    .

--- __manifest__.py
+++ __manifest__.py
@@ -23,7 +23,7 @@
         #'views/account_condominium_table_view.xml',
         'views/account_condominium_view.xml',
         #'views/account_move_form_btn.xml',
-        #'views/menu.xml',
+        'views/menu.xml',
     ],
     'demo': [
         #'demo/account_demo.xml',
