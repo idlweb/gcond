5e6ed526374901cb473fb4cf48ce4c7af1e0e5cd
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 11:54:22 +0200

    .

--- models/condominio.py
+++ models/condominio.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class AccountCondominium(models.Model):
     _inherit = 'res.partner'
 
+    """
     condominium_id = fields.Many2one(
         comodel_name='account.condominio',
         string='Condominium',
@@ -13,3 +14,4 @@ class AccountCondominium(models.Model):
         inverse_name='condominium_id',
         string='Related Condominiums',
     )
+    """
