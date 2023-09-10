c309fe42e84208aad18cccc0e010860c8117c722
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 11:36:03 +0200

    .

--- models/account_condominio_table.py
+++ models/account_condominio_table.py
@@ -6,7 +6,7 @@ class AccountCondominiumTable(models.Model):
     name = fields.Char(string='Name', required=True)
     code = fields.Char(string='Code', required=True)
     description = fields.Text(string='Description')
-    condominium_id = fields.Many2one('account.condominio', string='Condominium')
+    #condominium_id = fields.Many2one('account.condominio', string='Condominium')
     code_table = fields.Char(string='Code Table', required=True)
     unit_of_measure = fields.Char(string='Unit Of Measure')
     value_distribution = fields.Float(string='Value Distribution')
