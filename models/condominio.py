13b34577f5c4ffc704cc52e3108dfd13ef6bb0f9
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 15:12:45 +0200

    modificato il file sbngliato

--- models/condominio.py
+++ models/condominio.py
@@ -9,6 +9,20 @@ class AccountCondominium(models.Model):
         string='Condominium',
     )
  
+    related_condominiums = fields.One2many(
+        comodel_name='res.partner',
+        inverse_name='condominium_id',
+        string='Related Condominiums',
+    )
+ 
+    tax_ids = fields.Many2many(
+        comodel_name='account.tax',
+        relation='account_condominio_tax_rel',
+        column1='condominio_id',
+        column2='tax_id',
+        string='Taxes',
+    )
+    
     """
     related_condominiums = fields.One2many(
         comodel_name='res.partner',
