259ba9c8a69ba8a874951b2baee132bacdd73235
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 18:08:20 +0200

    .

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@
 from . import account_condominio
 from . import account_condominio_table
 from . import condomino
-#from . import account_move_condominio
+from . import account_move_condominio
 #from . import partner
 
 
