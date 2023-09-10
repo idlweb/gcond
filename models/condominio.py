287bd74ae7e4edfe1a5cc63e9ad940148758f076
Antonio <antonio.vangi.av@gmail.com>
2023-09-10 11:52:57 +0200

    related_ e  inverse_name condominium_id

--- models/condominio.py
+++ models/condominio.py
@@ -3,6 +3,10 @@ from odoo import models, fields, api
 class AccountCondominium(models.Model):
     _inherit = 'res.partner'
 
+    condominium_id = fields.Many2one(
+        comodel_name='account.condominio',
+        string='Condominium',
+    )
  
     related_condominiums = fields.One2many(
         comodel_name='res.partner',
