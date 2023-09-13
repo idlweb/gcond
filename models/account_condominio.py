551604cc23475d98de061ded031460b31962fd41
Antonio <antonio.vangi.av@gmail.com>
2023-09-13 21:29:23 +0200

    .

--- models/account_condominio.py
+++ models/account_condominio.py
@@ -15,12 +15,12 @@ class GcondAccountCondominium(models.Model):
     zip = fields.Char(string='ZIP', required=True)
     phone = fields.Char(string='Phone')
     
-    """
+    
     type_registration = fields.Selection(
         [('fattura', 'Fattura'), ('ricevuta', 'Ricevuta')],
         string='Tipo Registrazione',
         default='fattura',)
-    """
+    
     
     """
     journal_id = fields.Many2one(
