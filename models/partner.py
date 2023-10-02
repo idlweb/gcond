1b9f91b13ca4a64c60ed707dd101fa779f25a4e9
Antonio <antonio.vangi.av@gmail.com>
2023-10-02 11:46:29 +0200

    .

--- models/partner.py
+++ models/partner.py
@@ -4,20 +4,27 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = "res.partner" 
 
-    """
-    # condomino
-    condomino = fields.Boolean(string="Condomino")
-    
-    # Proprietario
-    proprietario = fields.Boolean(string="Proprietario")
-
-    # Tabella di Ripartizione
-    """
-
-    """
-    tabella_ripartizione_id = fields.Many2one(
-        "gcond.tabelle_ripartizione", string="Tabella di Ripartizione"
+    condomino_id = fields.Many2one(
+        comodel_name='account.condomino',
+        string='Condominio',
+        ondelete='set null',
     )
-    
+
+
+    type_condomino = fields.Selection(
+        [('affuttuario', 'Affittuario'), ('proprietario', 'Proprietario')],
+        string='Tipologia condomino',
+        default='proprietario',)
+
+
+    """
+    # fix many2many da 'estensione ereditaria'
+    tax_ids = fields.Many2many(
+        comodel_name='account.tax',
+        relation='account_condominio_tax_rel',
+        column1='condominio_id',
+        column2='tax_id',
+        string='Taxes', 
+    )
     """
     
