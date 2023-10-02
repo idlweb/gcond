09547fe89d760940a0f698bf24d5da19d4e99d48
Antonio <antonio.vangi.av@gmail.com>
2023-10-02 23:11:47 +0200

    .

--- models/account_condomino.py
+++ models/account_condomino.py
@@ -25,13 +25,36 @@ class GcondAccountCondomino(models.Model):
     channel_ids = fields.Many2many(
         relation='account_condomino_mail_partner_rel',
     )
-  
+
+    """
+    def action_open_condominio_form(self):
+        view_id = self.env.ref('gcond.view_condomino_form').id
+
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'account.condomino',
+            'views': [(view_id, 'form')],
+            'res_id': self.id,
+            'target': 'current',
+        }
+    
+    def post_init(self):
+        for condomino in self:
+            if condomino.action_open_condominio_executed:
+                self.env['log'].info("=============>    La funzione action_open_condominio è stata eseguita per il condomino con ID", condomino.id)
+    """
+
+
     @api.model
     def create(self, vals):
+        # Imposta il valore del campo `commercial_partner_id` su un valore valido.
+        if not vals.get('commercial_partner_id'):
+            vals['commercial_partner_id'] = self.env['res.partner'].create({'name': 'Condominio'})
         #Crea un nuovo condominio.
         _logger.debug('===============================> Create a %s with vals %s', self._name, vals)
         record = super(GcondAccountCondomino, self).create(vals)  
-        self.commercial_partner_id = self.create({'name': 'test'})
             
         return record
 
