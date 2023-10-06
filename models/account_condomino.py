65032dfa1bc797ea1e07b89d04653b196f6c1fd3
Antonio <antonio.vangi.av@gmail.com>
2023-10-06 21:26:12 +0200

    .

--- models/account_condomino.py
+++ models/account_condomino.py
@@ -61,23 +61,22 @@ class GcondAccountCondomino(models.Model):
             if partner.is_company or not partner.parent_id:
                 partner.commercial_partner_id = partner.env['account.condomino'].search([('name', '=', partner.name)], limit=1)                    
     """
-    @api.constrains('name')
-    def _check_name(self):
-        for partner in self:
-            if not partner.name:
-                raise ValidationError("I condomini richiedono un nome")
+    _sql_constraints = []
             
     @api.depends('is_company', 'parent_id.commercial_partner_id')
     def _compute_commercial_partner(self):
         for partner in self:
             if partner.is_company or not partner.parent_id:
+                if not partner.name:
+                    partner.name = "Nominativo condomino"
                     condomino = partner.env['res.partner'].search([('name', '=', partner.name)], limit=1)
                     partner.commercial_partner_id = self.env['account.condomino'].create({
                         'name': condomino.name,
                         'is_company': condomino.is_company,
                         'parent_id': condomino.parent_id,
                     })
- 
+    
+
     """
     def action_open_condominio_form(self):
         view_id = self.env.ref('gcond.view_condomino_form').id
