62b60f535114b2a9bc93644a52113679dd687ef9
Antonio <antonio.vangi.av@gmail.com>
2023-10-08 17:19:42 +0200

    .

--- models/account_condominio_table.py
+++ models/account_condominio_table.py
@@ -3,13 +3,6 @@ from odoo import models, fields, api
 class AccountCondominiumTable(models.Model):
     _name = 'account.condominio.table'
 
-    table_id = fields.Many2one(
-        'account.condominio.table.master',
-        string='testata',
-        required=False,
-        inverse='',
-    )
-
     unit_of_measure = fields.Char(string='Unit Of Measure')
     value_distribution = fields.Float(string='Value Distribution')
     quote = fields.Float(string='percentuale di competenza')
