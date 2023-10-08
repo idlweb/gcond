1108c6bd420180c172bc01ee6e460353c6fc69e1
Antonio <antonio.vangi.av@gmail.com>
2023-10-09 01:34:11 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -49,7 +49,6 @@ class AccountCondominioTableMaster(models.Model):
             """
             # Ottieni tutte le righe di dettaglio
             dettagli = self.env['account.condominio.table'].search([('table_id', '=', self.id)])
-            print("=========================================>",dettagli)
             # Memorizza gli ID delle righe di dettaglio
             id_dettagli = {dettaglio.id for dettaglio in dettagli}
             # Elimina tutte le righe di dettaglio
