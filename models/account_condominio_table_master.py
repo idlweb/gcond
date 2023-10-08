384f16e1b9034600496758b44633433eca5a2f36
Antonio <antonio.vangi.av@gmail.com>
2023-10-08 23:19:04 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -43,6 +43,8 @@ class AccountCondominioTableMaster(models.Model):
             record = self.env['account.condominio.table'].create({
                 'table_id': self.id,
                 'condomino_id': condomino.id,
+                'quote' : 100,
+                'name'  : self.name,
             })
 
         return {}
