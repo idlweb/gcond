9ef7aa2849fd41f8a2532c1b32c51e7148decc00
Antonio <antonio.vangi.av@gmail.com>
2023-10-08 16:55:32 +0200

    .

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     'depends' : ['web','base','account'],
     'data': [
         'security/ir.model.access.csv',
-        'views/account_condominium_table_view.xml',
+        #'views/account_condominium_table_view.xml',
         'views/account_condominium_view.xml',
         'views/account_condomino_view.xml',
         #'views/account_move_form_btn.xml',
