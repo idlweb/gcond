59b1bd083a361d6317b5e97dd071565525b606c0
Antonio <antonio.vangi.av@gmail.com>
2023-10-09 01:16:38 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -49,8 +49,7 @@ class AccountCondominioTableMaster(models.Model):
             """
             # Ottieni tutte le righe di dettaglio
             dettagli = self.env['account.condominio.table'].search([('table_id', '=', self.id)])
-            print(dettagli)
-            assert dettagli == [], "La variabile 'dettagli' non è vuota."
+            print("=========================================>",dettagli)
             # Memorizza gli ID delle righe di dettaglio
             id_dettagli = {dettaglio.id for dettaglio in dettagli}
             # Elimina tutte le righe di dettaglio
