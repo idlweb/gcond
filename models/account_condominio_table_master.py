581809e0a08990cd927f3efb99cb715be99ede9a
Antonio <antonio.vangi.av@gmail.com>
2023-10-09 16:16:48 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -66,7 +66,8 @@ class AccountCondominioTableMaster(models.Model):
 
             # Memorizza gli ID delle righe di dettaglio
             id_dettagli = {dettaglio.id for dettaglio in dettagli}
-            self._logger.info('verifica esistenza dettagli:',id_dettagli)
+            _logger.info('verifica esistenza dettagli:',id_dettagli)
+            #_logger.debug('')
 
             # Elimina tutte le righe di dettaglio
             for dettaglio_id in id_dettagli:
