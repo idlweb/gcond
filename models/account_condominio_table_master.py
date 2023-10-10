a18586ad9aa67d3d805f9bed2285547345fd5900
Antonio <antonio.vangi.av@gmail.com>
2023-10-10 19:11:58 -0400

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -64,18 +64,17 @@ class AccountCondominioTableMaster(models.Model):
         else:
             # Ottieni tutte le righe di dettaglio
             dettagli = self.env['account.condominio.table'].search([('table_id', '=', self.id)])
+            _logger.info('=============INIZIO===================')
+            _logger.info(self.id) 
 
             # Memorizza gli ID delle righe di dettaglio
             id_dettagli = {dettaglio.id for dettaglio in dettagli}
-            _logger.info('verifica esistenza dettagli:')
-            _logger.info('=============INIZIO===================')
+            
             #_logger.debug('')
 
             # Elimina tutte le righe di dettaglio
             for dettaglio_id in id_dettagli:
-                dettaglio = self.env['account.condominio.table'].browse(dettaglio_id.id)
-                _logger.debug(dettaglio.id)
-                _logger.debug(pprint.pformat(dettaglio))      
+                dettaglio = self.env['account.condominio.table'].browse(dettaglio_id.id)                
                 dettaglio.unlink()
 
             _logger.info('verifica esistenza dettagli:')
@@ -83,6 +82,9 @@ class AccountCondominioTableMaster(models.Model):
             _logger.info(list(id_dettagli))    
             _logger.info('==============FINE=================')
             
+            # Esegui una commit manuale
+            self.flush()
+
             # Ripopola le righe di dettaglio
             condomini = self.env['res.partner'].search([('condominio_id', '=', self.condominio_id.id)])
             for condomino in condomini:
