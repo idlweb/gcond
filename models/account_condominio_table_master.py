ad4bf3129742d2d748e588eeb9d331f737b97c92
Antonio <antonio.vangi.av@gmail.com>
2023-10-16 02:13:21 -0400

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -81,8 +81,9 @@ class AccountCondominioTableMaster(models.Model):
             if self.condominio_id != self.condominio_id_old:
                 _logger.info('==============DEBUG=================')          
                 _logger.info('il valore di condominio è %s, quello di id_old è %s', self.condominio_id, self.condominio_id_old)
-                self.condominio_id_old = self.condominio_id 
-                self.flush()
+        
+        self.condominio_id_old = self.condominio_id 
+        self.flush()
 
         return {}
 
