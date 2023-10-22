6de2d33a4f1d667c50824505b4ed5d3088e34426
Antonio <antonio.vangi.av@gmail.com>
2023-10-22 11:03:20 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -79,7 +79,7 @@ class AccountCondominioTableMaster(models.Model):
             pass
         else:
             if self.condominio_id != self._origin.condominio_id:       # _origin è il valore precedente, condominio_id il new                    
-                _logger.info('il valore di condominio è %s, quello di id_old è %s', self.condominio_id, self._origin.condominio_id)
+                _logger.info('il valore di condominio è %s, quello precedente è %s', self.condominio_id, self._origin.condominio_id)
                 condomini = self.env['res.partner'].search([('condominio_id.id', '=', self.condominio_id)])
 
                 #self.write({'table_ids': []})    
