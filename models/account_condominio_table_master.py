9dceaa15dbab77d125f6101f3efc4d4439608e1d
Antonio <antonio.vangi.av@gmail.com>
2023-10-22 12:00:08 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -100,7 +100,7 @@ class AccountCondominioTableMaster(models.Model):
 
         #self.write({'condominio_id_old': 999})
         #self.condominio_id_old = self.condominio_id 
-        #self.flush()
+        self.flush()
         
 
         return {}
