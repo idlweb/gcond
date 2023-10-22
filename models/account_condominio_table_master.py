7b01166b450a63aa2b87c694ae100ca1932b8ffc
Antonio <antonio.vangi.av@gmail.com>
2023-10-22 11:15:59 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -78,7 +78,7 @@ class AccountCondominioTableMaster(models.Model):
             # Se il condominio_id non è impostato, disabilitiamo la funzione onchange
             pass
         else:
-            if self.condominio_id != self._origin.condominio_id:       # _origin è il valore precedente, condominio_id il new                    
+            #if self.condominio_id != self._origin.condominio_id:       # _origin è il valore precedente, condominio_id il new                    
                 _logger.info('il valore di condominio è %s, quello precedente è %s', self.condominio_id, self._origin.condominio_id)
                 condomini = self.env['res.partner'].search([('condominio_id.id', '=', self.condominio_id.id)])
 
