df980682e3fe447266fb0d232c6067d0ef322aa5
Antonio <antonio.vangi.av@gmail.com>
2023-10-22 11:21:13 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -73,27 +73,28 @@ class AccountCondominioTableMaster(models.Model):
 
     @api.onchange('condominio_id')
     def onchange_condominio_id(self):
-        _logger.info('==============DEBUG=================2')  
+        _logger.info('==============DEBUG=================2') 
+        _logger.info('il valore di condominio fuori è %s', self.condominio_id)
         if not self.condominio_id:
-             _logger.info('il valore di condominio è %s', self.condominio_id)
+             _logger.info('il valore di condominio quando non modificato è %s', self.condominio_id)
             # Se il condominio_id non è impostato, disabilitiamo la funzione onchange
         else:
             #if self.condominio_id != self._origin.condominio_id:       # _origin è il valore precedente, condominio_id il new                    
-                _logger.info('il valore di condominio è %s, quello precedente è %s', self.condominio_id, self._origin.condominio_id)
-                condomini = self.env['res.partner'].search([('condominio_id.id', '=', self.condominio_id.id)])
+            _logger.info('il valore di condominio è %s, quello precedente è %s', self.condominio_id, self._origin.condominio_id)
+            condomini = self.env['res.partner'].search([('condominio_id.id', '=', self.condominio_id.id)])
 
-                #self.write({'table_ids': []})    
-                #self.table_ids = []
-                self.table_ids.unlink()
-                #self.flush()
-                # Ripopola le righe di dettaglio
-                
-                for condomino in condomini:
-                    record = self.env['account.condominio.table'].create({
-                        'table_id': self.id,
-                        'condomino_id': condomino.id,
-                        'quote' : 100.00,
-                    })
+            #self.write({'table_ids': []})    
+            #self.table_ids = []
+            self.table_ids.unlink()
+            #self.flush()
+            # Ripopola le righe di dettaglio
+            
+            for condomino in condomini:
+                record = self.env['account.condominio.table'].create({
+                    'table_id': self.id,
+                    'condomino_id': condomino.id,
+                    'quote' : 100.00,
+                })
         #self.condominio_id_old = 99
         #self.write({'condominio_id_old': 999})
         #self.condominio_id_old = self.condominio_id 
