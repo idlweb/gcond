ef36a9819d993ede1600be81454fb0e2e40b297d
Antonio <antonio.vangi.av@gmail.com>
2023-10-23 09:28:20 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -73,8 +73,7 @@ class AccountCondominioTableMaster(models.Model):
 
     @api.onchange('condominio_id')
     def onchange_condominio_id(self):
-         prrrr
-        _logger.info('==============DEBUG=================2') 
+        _logger.info('==============DEBUG=================3') 
         _logger.info('^^^^^^^^^^^^^^^^^il valore di condominio fuori è %s', self.condominio_id)
         if not self.condominio_id:
              pass
