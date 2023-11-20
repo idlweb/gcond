5db12d252c63116e7de58b7e6fc4326818585923
Antonio <antonio.vangi.av@gmail.com>
2023-11-20 09:58:12 +0100

    .

--- controllers /saldo.py
+++ controllers /saldo.py
@@ -4,10 +4,7 @@ from odoo import http
 from odoo.http import request
 
 
-class Main(http.Controller):
+class Conto(http.Controller):
     @http.route('/mio-conto', type='http', auth="user", website=True)
     def gcond_saldo(self):
-        return request.render(
-            'gcond.mio-conto', {
-                'conti': request.env['account.move'].search([]),
-            })
+       
