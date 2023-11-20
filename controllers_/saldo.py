455b3984ef26bc4fcaf595e12155b27f3f65004a
Antonio <antonio.vangi.av@gmail.com>
2023-11-20 13:06:58 +0100

    .

--- controllers_/saldo.py
+++ controllers_/saldo.py
@@ -11,3 +11,20 @@ class Saldo(http.Controller):
             'gcond.mio-conto', {
                 'saldo': request.env['account.move'].search([]),
             })
+
+    @http.route('/books/submit_pag', type='http', auth="user", website=True)
+    def saldo_pago(self, **post):
+        if post.get('book_id'):
+            conto_id = int(post.get('conto_id'))
+            pagamento = post.get('issue_description')
+            request.env['account.move'].sudo().create({
+                'id': conto_id,
+                'name': pagamento,
+                #'submitted_by': request.env.user.id
+            })
+            return request.redirect('/saldo/submit_pag?submitted=1')
+
+        return request.render('gcond.conto_pag_form', {
+            'saldo': request.env['account.move'].search([]),
+            'submitted': post.get('submitted', False)
+        })
