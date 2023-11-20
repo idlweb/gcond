cbf77c07d85960da4b6580b1ea2c92cf759d91ce
Antonio <antonio.vangi.av@gmail.com>
2023-11-20 09:59:53 +0100

    .

--- controllers /saldo.py
+++ controllers /saldo.py
@@ -4,7 +4,7 @@ from odoo import http
 from odoo.http import request
 
 
-class Conto(http.Controller):
+class Saldo(http.Controller):
     @http.route('/mio-conto', type='http', auth="user", website=True)
     def gcond_saldo(self):
-       
+        pass
