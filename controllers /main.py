fce9425188985508776d2a3568a0bdb1d1df8c4c
Antonio <antonio.vangi.av@gmail.com>
2023-11-20 09:44:49 +0100

    .

--- controllers /main.py
+++ controllers /main.py
@@ -8,6 +8,5 @@ class Main(http.Controller):
     @http.route('/mio-conto', type='http', auth="user", website=True)
     def gcond_saldo(self):
         return request.render(
-            'gcond.mio-conto', {
-                'conti': request.env['account.move'].search([]),
-            })
+            pass,
+            )
