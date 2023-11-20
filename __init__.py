85d01829251c94e9da3983708e2ae9b1e6d390ba
Antonio <antonio.vangi.av@gmail.com>
2023-11-20 09:55:05 +0100

    .

--- __init__.py
+++ __init__.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import controllers
 from . import models
+from . import controllers
+
 #from . import views
 #from . import security
 #from . import demo
--- controllers /main.py
+++ controllers /main.py
@@ -7,4 +7,7 @@ from odoo.http import request
 class Main(http.Controller):
     @http.route('/mio-conto', type='http', auth="user", website=True)
     def gcond_saldo(self):
-       pass
+        return request.render(
+            'gcond.mio-conto', {
+                'conti': request.env['account.move'].search([]),
+            })
