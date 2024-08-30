bea90395c2bb53aa8db4da39e2053c4f19ffd42f
Antonio <antonio.vangi.av@gmail.com>
2024-08-30 10:24:22 +0200

    .

--- models/account_move_condominio.py
+++ models/account_move_condominio.py
@@ -1,11 +1,9 @@
 """
-    With this Module we can use all thr features of the account.move 
-    and get the parameters to pass theme at 'distribuite_charges'
+    With this Module we can use all the features of the account.move 
+    and get the parameters to pass them to 'distribute_charges'
 """
 
-
 from odoo import models, fields, api
-
 from . import account_condominio_table_master
 
 class AccountMove(models.Model):
@@ -20,7 +18,7 @@ class AccountMove(models.Model):
         charges = []
 
         # Check if the condominium is included in the distribution table.
-       if table.get(self.id) is None :
+        if table.get(self.id) is None:
             raise ValueError('The condominium is not included in the distribution table.')
 
         # Calculate the condominium's share of the charge.
@@ -39,63 +37,23 @@ class AccountMove(models.Model):
                 },
                 {
                     'account_id': account_id,
-                    'name': 'Spese condominiali',
+                    'name': self.name,
                     'credit': charge,
                 },
             ],
         })
 
-        # Add the charge to the list of charges.
         charges.append(account_move)
-
         return charges
 
-
     def button_distribute_charges(self):
-        #Aggiunta la verifica sullo stato bozza 
-        if self.state != 'draft':
-            raise ValueError('The invoice must be in draft state.')
-        
-        move = self
+        if self.state != 'posted':
+            raise UserError('The invoice must be posted before distributing charges.')
+
+        # Example parameters, these should be dynamically determined
         amount = self.amount_total
-        details = self.invoice_line_ids
-        account_id = details.account_id
+        table = self.env['account.condominio.table'].get_distribution_table(self.id)
+        document_number = self.name
+        account_id = self.env['account.account'].search([('code', '=', '400000')], limit=1).id
 
-        # TO-DO da dove prendo self.code_table 
-        # Ip.n1 -> lo prendiamo dal context (vediamo quando
-        # inserirlo)
-        """
-            move.id -> journal_id -> condominio_id
-
-        """
-        table = self.env['account.condominio.table.master'].search([('account_id', '=', self.id)], limit=1)
-        document_number = self.ref
-        charges = self.distribute_charges(amount, table, document_number, account_id)        
-
-
-        """
-            Una nuova forma di dialogo con l'utente...
-        """
-        if charges:
-            message = 'The expenses have been successfully distributed to the condominiums.'
-        else:
-            message = 'There are no condominiums to distribute the expenses to.'
-
-        #return charges
-
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'params': {
-                'title': 'Expenses Distribution',
-                'message': message,
-                'type': 'info',
-            },
-        }
-
-        
-   
-
-    
-
-        
+        self.distribute_charges(amount, table, document_number, account_id)
