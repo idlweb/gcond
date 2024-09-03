3aa0d045730df2ff06036dabfc54c556ca2ed4a9
Antonio <antonio.vangi.av@gmail.com>
2024-09-03 21:58:58 +0200

    .

--- models/account_move_condominio.py
+++ models/account_move_condominio.py
@@ -5,12 +5,13 @@
 
 from odoo import models, fields, api
 #from . import account_condominio_table_master
-from odoo.exceptions import ValidationError, UserError
+
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     distribution_table_id = fields.Many2one('account.condominio.table', string='Distribution Table')
+    
     """
         sembra corretto anche il ricorso al 'code_table'
         ottengo la tabella intera ma il code_table da dove lo vado a prendere?
