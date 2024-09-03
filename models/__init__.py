9e1c97f4db54baea26ff17080a7acf7301974df4
Antonio <antonio.vangi.av@gmail.com>
2024-09-03 21:46:34 +0200

    .

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,8 @@
 from . import account_condominio
 
 from . import account_condominio_table
-from . import account_condominio_table_master
-"""
+#from . import account_condominio_table_master
+
 from . import account_move_condominio
 from . import account_condomino
 from . import account_journal_condominio
@@ -12,7 +12,7 @@ from . import account_sensore
 from . import ir_action_act_window
 from . import ir_ui_view
 from . import modello
-"""
+
 
 
 
