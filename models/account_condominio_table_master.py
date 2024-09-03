6b2c8d0a19e95647635fe05434b632e63aaad507
Antonio <antonio.vangi.av@gmail.com>
2024-09-03 22:36:57 +0200

    .

--- models/account_condominio_table_master.py
+++ models/account_condominio_table_master.py
@@ -20,7 +20,7 @@ class AccountCondominioTableMaster(models.Model):
     name = fields.Char(string='Name', required=True)
     code_table = fields.Char(string='Codice tabella')
     description = fields.Char(string='Descrizione')
-    #account_ids = fields.One2many('account.account', 'distribution_table_id', string='Accounts')
+    account_ids = fields.One2many('account.account', string='Accounts')
     #account_id = fields.Many2one('account.account', string='Conto di contabilità')
     
     condominio_id = fields.Many2one(
