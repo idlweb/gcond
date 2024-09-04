7f314d8c29c0cfcc3becd710ed2660f28031ae76
Antonio <antonio.vangi.av@gmail.com>
2024-09-04 16:43:39 +0200

    .

--- models/account_move_condominio.py
+++ models/account_move_condominio.py
@@ -32,7 +32,7 @@ class AccountMove(models.Model):
   
             # Get the amount of the cost entry
             amount = line.debit
-            raise UserError(line.account_id.id)
+            raise UserError(line.account_id)
             # Get the account_condominio_table_master record associated with the debit/cost entry
             account_condominio_table = self.env['account.condominio.table.master'].search([
                 ('condominio_id', '=', condominio_id),
