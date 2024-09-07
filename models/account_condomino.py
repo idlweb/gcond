7b3d4a1b67caaa1529c1fca54e552ffaa745adaf
Antonio <antonio.vangi.av@gmail.com>
2024-09-07 12:32:52 +0200

    .

--- models/account_condomino.py
+++ models/account_condomino.py
@@ -29,7 +29,11 @@ class GcondAccountCondomino(models.Model):
         selection=[('person', 'Individual'), ('company', 'Company'), ('condomino','Condomino')],
         compute='_compute_company_type', inverse='_write_company_type')
     
-
+    account_id = fields.Many2one(
+        comodel_name='account.account',
+        string='Account',
+        ondelete='set null',
+    )
     
 
     @api.depends('is_company')
@@ -60,6 +64,21 @@ class GcondAccountCondomino(models.Model):
     
 
 
+    @api.model
+    def create(self, vals):
+        condominio = self.env['account.condominio'].browse(vals['condominio_id'])
+        partner = super(GcondAccountCondomino, self).create(vals)
+        if partner.is_condominio:
+            ass_account = self.env['account.account'].create({
+                'name': f"{partner.name}-{condominio.name}",
+                'code': self.env['ir.sequence'].next_by_code('account.account'),
+                'user_type_id': self.env.ref('account.data_account_type_receivable').id,
+                'reconcile': True,
+                'company_id': partner.company_id.id,
+                'condominio_id': partner.condominio_id.id,
+            })
+            partner.account_id = ass_account.id
+        return partner
 
     """
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
