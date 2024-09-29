91c1498d89572b6a592b3e6d359bb042ad998e15
Antonio <antonio.vangi.av@gmail.com>
2024-09-29 22:38:28 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -6,7 +6,6 @@ class AccountBankStatement(models.Model):
 
     amount_consumed = fields.Boolean(string='Importo Consumato', default=False)
 
-    @api.mmodel
     def action_consume_payment(self):
         for statement in self:
             for line in statement.line_ids:
