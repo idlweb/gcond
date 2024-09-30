ddaa712cb90916f96425775a4bfa1f6722bf09dd
Antonio <antonio.vangi.av@gmail.com>
2024-09-30 13:15:41 +0200

    .

--- models/__init__.py
+++ models/__init__.py
@@ -10,7 +10,7 @@ from . import account_sensore
 from . import ir_action_act_window
 from . import ir_ui_view
 from . import modello
-from . import condominio_banca
+#from . import condominio_banca
 
 
 
