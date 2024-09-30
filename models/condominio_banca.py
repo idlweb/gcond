242c395a8983deb0227a4c0b71f2a56984993ee8
Antonio <antonio.vangi.av@gmail.com>
2024-09-30 13:06:08 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -5,12 +5,13 @@ class AccountBankStatement(models.Model):
     _inherit = 'account.bank.statement.line'
 
     amount_consumed = fields.Boolean(string='Importo Consumato', default=False)
+    amount_re
 
     def action_consume_payment(self):
         for statement in self:
-            for line in statement.line_ids:
+            for line in statement.line_ids:  # line -> account.move.line
                 
-                importo = statement.amount                
+                importo = statement.amount + statement.amount_residual               
                 partner = line.partner_id                    
                 if not partner:
                     raise UserError("Nessun partner associato a questa riga dell'estratto conto.")
@@ -20,21 +21,29 @@ class AccountBankStatement(models.Model):
                     ('account_id', '=', partner.conto_id.id),
                     ('move_id.payment_state', '!=', 'paid')
                 ])
+            
+                # Calcola la somma dei valori del campo 'debit' per le righe delle fatture non pagate
+                somma_quote = self.somma_quote_da_pagare(partner.conto_id.id)
                 #
                 #if not unpaid_lines:
                 #    raise UserError("Non ci sono quote non pagate per questo partner.")
 
-                # Applica il pagamento alle quote
-                amount_to_consume = importo
-                raise UserError(importo)
+                """
+                    logica di calcolo:
+                    -non ci sono valori residui da pagare ma valori residui da consumare
+                    -considerare se il valore da pagare è minore del valore residuo
+                    primo debito -> unpaid_line.debit o unpaid_line.balance
+                """
+
                 for unpaid_line in unpaid_lines:
-                    if amount_to_consume <= 0:
-                        break
-                    amount_to_pay = min(amount_to_consume, unpaid_line.amount_residual)
-                    unpaid_line.amount_residual -= amount_to_pay
-                    amount_to_consume -= amount_to_pay
-                    if unpaid_line.amount_residual <= 0:
+                    if importo  >= unpaid_line.balance:
+                        importo = importo - unpaid_line.debit 
                         unpaid_line.move_id.payment_state = 'paid'
+                    else:
+                        if importo >= 0:
+                            statement.amount_residual = importo
+                        break
+                  
 
                 # Aggiorna lo stato della riga dell'estratto conto
                 statement.amount_consumed = True
@@ -61,3 +70,15 @@ class AccountBankStatement(models.Model):
                 }
                 self.env['account.move'].create(move_vals)
                 """
+
+    def somma_quote_da_pagare(id):
+        # Calcola la somma dei valori del campo 'debit' per le righe delle fatture non pagate
+        debit_sum = self.env['account.move.line'].read_group(
+            domain=[
+                ('account_id', '=',id),
+                ('move_id.payment_state', '!=', 'paid')
+            ],
+            fields=['debit:sum'],
+            groupby=[]
+        )[0]['debit']
+        return debit_sum
