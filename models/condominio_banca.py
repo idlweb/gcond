dc92730ef93d3124b29455dfc776fd2a060c59f1
Antonio <antonio.vangi.av@gmail.com>
2024-09-30 16:23:17 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -29,8 +29,8 @@ class AccountBankStatement(models.Model):
                 #    raise UserError("Non ci sono quote non pagate per questo partner.")
 
                 
-                debug[0] = somma_quote
-                debug[1] = importo
+                #debug[0] = somma_quote
+                #debug[1] = importo
                 
                 """
                     logica di calcolo:
@@ -45,14 +45,14 @@ class AccountBankStatement(models.Model):
                     if importo  >= unpaid_line.debit:
                         importo = importo - unpaid_line.debit 
                         unpaid_line.move_id.payment_state = 'paid'
-                        debug[i] = unpaid_line.debit
-                        i++
+                        #debug[i] = unpaid_line.debit
+                        #i++
                     else:                        
                         if importo >= 0:
                             statement.amount_residual = importo
                         break
                     
-                raise UserError(debug)
+                #raise UserError(debug)
 
                 # Aggiorna lo stato della riga dell'estratto conto
                 statement.amount_consumed = True
