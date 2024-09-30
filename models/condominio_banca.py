972a0bbc79d65f336c434e1c3a2cf248969306ec
Antonio <antonio.vangi.av@gmail.com>
2024-09-30 13:08:04 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -35,14 +35,7 @@ class AccountBankStatement(models.Model):
                     primo debito -> unpaid_line.debit o unpaid_line.balance
                 """
 
-                for unpaid_line in unpaid_lines:
-                    if importo  >= unpaid_line.balance:
-                        importo = importo - unpaid_line.debit 
-                        unpaid_line.move_id.payment_state = 'paid'
-                    else:
-                        if importo >= 0:
-                            statement.amount_residual = importo
-                        break
+                
                   
 
                 # Aggiorna lo stato della riga dell'estratto conto
