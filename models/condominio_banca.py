1a69a9b09772ab911f03710cc32027f229b2cf53
Antonio <antonio.vangi.av@gmail.com>
2024-10-02 00:12:08 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -12,7 +12,7 @@ class AccountBankStatement(models.Model):
             if not statement.line_ids:
                 continue
 
-            importo = statement.amount + statement.amount_residual
+            importo = statement.amount + self.get_previous_residual(statement.line_ids[0].partner_id.id)         
             partner = statement.line_ids[0].partner_id
             if not partner:
                 raise UserError("Nessun partner associato a questa riga dell'estratto conto.")
@@ -34,6 +34,42 @@ class AccountBankStatement(models.Model):
                     break
 
             statement.amount_consumed = True
+
+            """
+            # Crea una scrittura contabile
+            move_vals = {
+                'journal_id': statement.journal_id.id,
+                'date': fields.Date.context_today(self),
+                'line_ids': [
+                    (0, 0, {
+                        'account_id': partner.property_account_receivable_id.id,
+                        'partner_id': partner.id,
+                        'credit': statement.amount,
+                        'debit': 0,
+                    }),
+                    (0, 0, {
+                        'account_id': self.env['account.account'].search([('name', 'ilike', partner.name)], limit=1).id,
+                        'partner_id': partner.id,
+                        'credit': 0,
+                        'debit': statement.amount,
+                    }),
+                ],
+            }
+            self.env['account.move'].create(move_vals)
+            """
+
+    def get_previous_residual(self, partner_id):
+        previous_statements = self.env['account.bank.statement.line'].search([
+            ('partner_id', '=', partner_id),
+            ('amount_consumed', '=', True)
+        ], order='date desc', limit=1)
+
+        if previous_statements:
+            residual = previous_statements.amount_residual
+            previous_statements.amount_residual = 0
+            return residual
+        return 0
+
 
     def somma_quote_da_pagare(self, account_id):
         result = self.env['account.move.line'].read_group(
