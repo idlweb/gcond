142e178387d794299eef9f86df60af979223d846
Antonio <antonio.vangi.av@gmail.com>
2024-10-07 00:47:30 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -31,7 +31,7 @@ class AccountBankStatement(models.Model):
                 raise UserError(somma_quote)
                 
                 # Aggiungi i valori di debug alla lista
-                debug.append("-somma_quote:"+str(somma_quote))
+                debug['somma quote'] = somma_quote
                 #debug.append("-importo estratto:"+str(importo))
 
                 """
