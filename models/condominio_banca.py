8b0519842c1bf81bf9b97fb17a2dc06b2568e2f9
Antonio <antonio.vangi.av@gmail.com>
2024-10-07 00:47:50 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -28,7 +28,7 @@ class AccountBankStatement(models.Model):
                 
                 # Calcola la somma dei valori del campo 'debit' per le righe delle fatture non pagate
                 somma_quote = self.somma_quote_da_pagare(partner.conto_id.id)
-                raise UserError(somma_quote)
+                #raise UserError(somma_quote)
                 
                 # Aggiungi i valori di debug alla lista
                 debug['somma quote'] = somma_quote
