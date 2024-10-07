81595ede320ed2d0473c4bf49899e925d0eb0b07
Antonio <antonio.vangi.av@gmail.com>
2024-10-07 02:35:03 +0200

    .

--- models/condominio_banca.py
+++ models/condominio_banca.py
@@ -45,7 +45,7 @@ class AccountBankStatement(models.Model):
                 for unpaid_line in unpaid_lines:
 
                     if importo >= unpaid_line.debit:
-                        unpaid_line.move_id.payment_state = 'paid'
+                        #unpaid_line.move_id.payment_state = 'paid'
                         importo -= unpaid_line.debit
                         debug['payment_state'+str(k)] = [unpaid_line.move_id.payment_state]
                         debug['riduzioni'+str(k)] = [importo]
